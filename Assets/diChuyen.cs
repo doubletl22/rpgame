7c1fb492af133502cfd344b1facbbf846e687bb6
doubletl22 <kiyoshineko41@gmail.com>
2025-10-23 18:29:15 +0700

    catmove

--- Assets/diChuyen.cs
+++ Assets/diChuyen.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class diChuyen : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Animator anim;
+    public int speed = 4;
+    public float walk;
+    public bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class diChuyen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        walk = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(walk * speed, rb.velocity.y);
+
+        if (isFacingRight == true && walk == -1)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            isFacingRight = false;
+        }
+        else if (isFacingRight == false && walk == 1)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            isFacingRight = true;
+        }
+
+        anim.SetFloat("isRunning", Mathf.Abs(walk));
+
     }
 }
